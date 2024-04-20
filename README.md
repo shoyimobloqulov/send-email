8f7d109c0448776aec3dfcab4d819b22ea111444
ShoyimObloqulov <obloqulovshoyim1@gmail.com>
2024-04-20 13:39:39 +0500

    Send Email and core function update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-## Send Email Message
+# Send emails with multiple To, CC, BCC, and Reply-to addresses
+### Yuborilgan xabar quydagicha ko'rinishga ega.
+![alt text](image.png)
+
+### Running
+<pre>git clone https://github.com/shoyimobloqulov/send-email</pre>
+## Ishga tushurish
+<pre>cd send-email</pre>
+<pre>php .\send-messsage.php</pre>
+
+## Batafsil tanishish uchun
+https://github.com/PHPMailer/PHPMailer
---
+++ core/send-messsage.php
@@ -0,0 +1,44 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    /*
+        password: jnmz ffcr wkvs zjrh
+        username or email: obloqulovshoyim1@gmail.com
+    */
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true; 
+    $mail->Username   = 'obloqulovshoyim1@gmail.com';    
+    $mail->Password   = 'jnmzffcrwkvszjrh';           
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; 
+    $mail->Port       = 465; 
+
+    $mail->setFrom('obloqulovshoyim1@gmail.com', 'Shoyim Obloqulov');
+    $mail->addAddress('obloqulovshoyim1@gmail.com', 'Shoyim Obloqulov');
+    $mail->addAddress('obloqulovshoyim1@gmail.com'); 
+    $mail->addReplyTo('obloqulovshoyim1@gmail.com', 'Shoyim Obloqulov');
+    $mail->addCC('obloqulovshoyim1@gmail.com');
+    $mail->addBCC('obloqulovshoyim1@gmail.com');
+
+    // Fayl yuborish
+    $mail->addAttachment('tmp/new-php-logo.png');
+
+    // Message Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Bu Subject';
+    $mail->Body    = 'Asosiy qism';
+    $mail->AltBody = 'Yuboriladigan xabar matni';
+
+    $mail->send();
+    echo 'Xabar yuborildi';
+} catch (Exception $e) {
+    echo "Xabar yuborilmadi. Xatolik: {$mail->ErrorInfo}";
+}
